434e2033d36bb12ba30450662c8248f0ea0f01a5
Dawood <dreamboy.dani@gmail.com>
2020-08-24 20:11:45 +0000

    Refactored middle and middleTest

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-}
-
-const assertArraysEqual = function (array1, array2) {
-  let emojiError = String.fromCodePoint(0x1F621);
-  let emojiCorrect = String.fromCodePoint(0x2705);
-
-  if (eqArrays(array1, array2)) {
-    console.log(`${emojiCorrect} Assertion Passed: [actual] === [expected]`);
-  } else {
-    console.log(`${emojiError} Assertion Failed: [actual] !== [expected]`);
-  }
-}
-
 const middle = function (array) {
 
   const middleElem = Math.floor(array.length / 2);
@@ -37,11 +13,4 @@ const middle = function (array) {
   return [array[middleElem - 1], array[middleElem]]
 }
 
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+module.exports = middle;
