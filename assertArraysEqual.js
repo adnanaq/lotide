726f94bb1afe43cb2947678fea1a13f7a8005580
Dawood <dreamboy.dani@gmail.com>
2020-08-24 20:07:32 +0000

    Refactored assertArraysEqual and its test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,14 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (array1, array2) {
   let emojiError = String.fromCodePoint(0x1F621);
   let emojiCorrect = String.fromCodePoint(0x2705);
-  const result = eqArrays(array1, array2)
 
-  if (result) {
+  if (eqArrays(array1, array2)) {
     console.log(`${emojiCorrect} Assertion Passed: [actual] === [expected]`);
   } else {
     console.log(`${emojiError} Assertion Failed: [actual] !== [expected]`);
   }
 }
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3])// => false
+module.exports = assertArraysEqual;
